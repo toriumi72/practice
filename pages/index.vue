aa515692998f681da1c8f090c8c9c988931e9cee
toriumi72 <106057295+toriumi72@users.noreply.github.com>
2022-12-04 11:55:18 +0900

    first

---
+++ pages/index.vue
@@ -0,0 +1,30 @@
+<template>
+<div>
+    <input type="text" v-model="message" />
+    <button @click="sendMessage">送信</button>
+    <ul>
+        <li v-for="message in messages" :key="message.id">{{ message.text }}    aaw        </li>
+    </ul>
+</div>
+</template>
+
+<script setup>
+const messages = ref([])
+const message = ref("") 
+
+const sendMessage = () => {
+    // 入力された文字列を取得する
+    const text = message.value;
+
+    // 入力フィールドを空にする
+    message.value = "";
+
+    // 入力された文字列をメッセージリストに追加する
+    messages.value.push({
+    id: Date.now(),
+    text
+    });
+
+    console.log(new Date());
+}
+</script>
